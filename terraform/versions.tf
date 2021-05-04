f99f57a82a2ad2f0b7fc28e28ae069218833bc0b
Duncan Bell <duncan.bell@madetech.com>
2021-05-04 11:19:39 +0100

    Upgrade from terraform 0.13 -> 0.15

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,22 +1,21 @@
 terraform {
-  required_version = ">= 0.15.0"
+  required_version = ">= 0.15"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.0"
     }
+    archive = {
+      source = "hashicorp/archive"
+    }
   }
 
   backend "s3" {
     bucket         = "madetech-sandbox-terraform-state"
-    key            = "bootstrap.tfstate"
+    key            = "sandbox.tfstate"
     region         = "eu-west-2"
     dynamodb_table = "sandbox-terraform-locks"
     encrypt        = true
   }
-}
-
-provider "aws" {
-  region = "eu-west-2"
 }
