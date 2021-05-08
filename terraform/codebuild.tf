ed688cb476223f1ca47475105c58b3e0b659bb61
Duncan Bell <duncan.bell@madetech.com>
2021-05-08 12:38:42 +0100

    Increase build timeout from 5->60mins

--- terraform/codebuild.tf
+++ terraform/codebuild.tf
@@ -7,7 +7,7 @@ resource "aws_codestarconnections_connection" "aws_nuke" {
 resource "aws_codebuild_project" "aws_nuke" {
   name           = local.name
   description    = "Nuke Sandbox Weekly"
-  build_timeout  = "5"
+  build_timeout  = "60"
   service_role   = aws_iam_role.codebuild.arn
   source_version = "main"
 
